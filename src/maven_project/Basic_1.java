b03730267b0c7797eda4f39b2370a46ef71fbd84
Gani <pragadaganesg143@gmail.com>
2024-09-06 12:22:19 +0530

    first commit

---
+++ src/maven_project/Basic_1.java
@@ -0,0 +1,312 @@
+package maven_project;
+
+public class Basic_1 {
+
+	public static void main(String[] args) {
+		/*Selenium WebDriver with Java -Basics to Advanced+Frameworks
+		Course image
+		24. Code download
+		"TOP RATED #1 Master SELENIUM java 4 (Latest) course" -5 Million students learning worldWide with great collaboration
+		Rating: 4.6 out of 5
+		4.6
+		118,717 ratings
+		382,566
+		Students
+		55.5 hours
+		Total
+		Last updated April 2024
+		English
+		English [Auto], Bulgarian [Auto]
+		, 
+		information alert
+		Schedule learning time
+		Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.
+		By the numbers
+		Skill level: All Levels
+		Students: 382566
+		Languages: English
+		Captions: Yes
+		Lectures: 335
+		Video: 55.5 total hours
+		Certificates
+		Get Udemy certificate by completing entire course
+
+		Features
+		Available on iOS and Android
+		Coding exercises
+		Description
+		Course last updated on April 15th with Latest set of Selenium TestNG Framework Interview questions
+
+		Have a Passion of learning Selenium but have no coding knowledge ? I will fulfill your wish with my easy teaching and life time query support  through QA Forum , Skype and Teamviewer
+
+		The one and only Selenium course (350+Lectures) in Udemy which conferred with the official BEST SELLER Tag and Highest Ratings -90,000+ 5 star Ratings)
+
+		Watchout Lecture 2 which depicts our roaring Success in Udemy with Success stories 
+		-Includes: 50+ Hours     lectures with real time projects + OnDemand Skype, Teamviewer sessions  (Life time support)  including Job Assistance    + 5 Million Students learning forum and 100 Pages detailed Material which you will not find in any other online course 
+
+		This course covers Java Basics core + Selenium Webdriver + Advanced Selenium + Interview Preparation + Framework design ( TestNG, ANT, Pageobject, Maven, Jenkins, Excel Datadriven, Cucumber, log4j ) + Selenium Grid + DataBase Testing + Performance Testing on Selenium scripts + Mobile testing Basics + Resume Preparation including       " Life Time Support (On Demand skype sessions) + Material + Job Assistance- (US, India, Canada
+
+		*One stop to become an Expert in Selenium from scratch *
+
+		I noticed other courses using the same content as our course. Please be aware of them
+
+		On course completion You will be Mastered in Selenium Automation Testing and implementing Successfully in your work place or you will land on High Paying Job 
+
+
+		    So what makes this course Unique in the Market?
+
+		We assume that students have no experience in automation / coding and start every topic from scratch and basics.
+
+		Examples are taken from  REAL TIME HOSTED WEB APPLICATIONS  to understand how different components can be automated.
+
+		  Topics includes: 
+		Complete Core java
+
+		Selenium Webdriver- Major focus (20 hours+ content)
+
+		 Live Examples in Automating Web applications in all supported Browsers
+
+		 Strategy to handle Rich Web UI using Advanced WebDriver
+
+		 Real time challenges we face in WebDriver and solutions to handle it
+
+		Selenium Grid
+
+		Live projects on Selenium 
+
+		Interview questions discussion with solutions
+
+		TestNG
+
+		Maven
+
+		ANT
+
+		Jenkins
+
+		Log4j
+
+		Test Execution reports
+
+		Cucumber
+
+		Data driven framework
+
+		Hybrid Framework
+
+		Page object Model Framework
+
+		DataBase testing with Selenium
+
+		Performance testing with Selenium
+
+		Mobile Automation basics only!
+
+		CLoud Automation with Sauce labs
+
+		Framework Design from scratch (       TestNG, ANT, PageObject, Maven, Jenkins, Excel , Datadriven, Cucumber, log4j ,Client html reports) 
+
+		All the Best! Happy Testing :)
+
+		What you’ll learn
+		*****By the end of this course,You will be Mastered on Selenium Webdriver with strong Core JAVA basics
+		****You will gain the ability to design PAGEOBJECT, DATADRIVEN&HYBRID Automation FRAMEWORKS from scratch
+		*** InDepth understanding of real time Selenium CHALLENGES with 100 + examples
+		*Complete knowledge on TestNG, MAVEN,ANT, JENKINS,LOG4J, CUCUMBER, HTML REPORTS,EXCEL API, GRID PARALLEL TESTING
+		* Specialized Trainers who can help in answering students queries in part of your Course Journey
+		"Additional knowledge to understand Mobile Testing Database testing and Performance testing with Selenium
+		You will be in a position to pick any website over internet and can automate it with all the possible test cases
+		You will improve your coding skills in writing best optmized code for Automating testcases
+		"Last but not least" you can clear any Interview and can Lead Entire Selenium Automation Project from Design Stage
+		You should be able to verify Data Integrity with the GUI of the application
+		You will be able to create GREAT LOOKING REPORTS to impress Clients
+		Deeper understanding of Automation guidelines and code quality standards
+		Includes a Practice Website to play around with all available Web driver functions to automate
+		Are there any course requirements or prerequisites?
+		******You need NOT have java coding experience to start this course********* Even non Programming candidates can follow this course comfortably
+		You will have Life Time instructor support to get solved all your queries. You can post your doubts in discussion board and we will get back with answer in less than 12 hours
+		All Installation setup including Java knowledge is taken care as part of course
+		Course includes real time projects with practical Solutions for the Robust Selenium Framework building
+		Theoretical Material,Code dump and Interview Guide are available for download
+		New : JOB ASSISTANCE after completion of course to make your Profile reach to Hundreds of Recruiters in my network.******
+		Join in our Selenium Training community with (350 + lectures, 5 Million Students) Learning Together which you will not see in any other Selenium online course on Udemy.
+		We have 5 dedicated Trainers who can help in answering students queries along with resume preparation , Interview preparation and Job assistance
+		***Honored with BEST SELLER Tag by Udemy for our most trusted commitment to students***
+		**"40,000+ ratings with an average 4.6/5"** which you will never see for any other course in Udemy
+		Who this course is for:
+		Manual testers, Non-programming aware testers interested in learning Automation
+		Freshers/Graudates who want to kick start their Carrier in IT World
+		Instructor
+
+		Rahul Shetty
+		Teacher over 800K+ QA Students|Founder of RahulShettyAcademy
+
+		"Nothing is Impossible. It all depends on how we are Trained on it."
+
+		"Teaching is my Passion. And it's my Profession. The only Business I know is Spreading Knowledge."
+
+		I'm Rahul Shetty -a QA instructor with 15 years of experience with over 800,000 QA students from 195 countries in my courses on Selenium, Software Testing (Jira), API Testing, Cypress, Postman, Appium, Playwright, JMeter, and many more...
+
+		I'm also the Lead Instructor at the Top QA Ed Tech Platform (Rahul Shetty Academy) where I helped hundreds of thousands of students learn to test and automate and change their lives by becoming an Automation Engineer.
+
+
+
+		Every QA Professional have the wish to learn trending Automation courses but somehow 90% of them are giving up their thoughts
+
+
+
+		It's not due to the lack of courses available in the Market, but it's about finding the right mentor who can guide, teach and understand student's pulse with their background education.
+
+		As a QA engineer myself with nearly two decades of experience in the software testing industry, I understand the QA mindset, how best we learn and based on that knowledge and experience, I have strategically built my courses, so my students get maximum benefit and career growth after taking my courses. I believe I've cracked the "formula" for teaching automation testing skills, and I'm really excited to finally share my expertise with you. I can confidently say that my online courses are without a doubt the most comprehensive ones on the market.
+
+
+
+
+
+		By joining my courses, you are not only getting the most practical, up to date, job relevant learning materials but also access to a lifelong mentor who has impacted and levelled up the careers of hundreds of thousands of QA engineers like You!
+
+
+
+
+
+		There isn't a day when I don't receive student success emails from across the globe about landing a new job, how my courses have changed their lives and career for the better and how they are respected and appreciated at the workplace after gaining new knowledge and experience from my course.
+
+		Teach the world online
+		Create an online video course, reach students across the globe, and earn money
+		Top companies choose Udemy Business to build in-demand career skills.
+		NasdaqVolkswagenBoxNetAppEventbrite
+		Udemy Business
+		Teach on Udemy
+		Get the app
+		About us
+		Contact us
+		Careers
+		Blog
+		Help and Support
+		Affiliate
+		Investors
+		Terms
+		Privacy policy
+		Sitemap
+		Accessibility statement
+		© 2024 Udemy, Inc.
+		24. Code download
+		Code download
+
+
+		public class CoreJavaBrushUp1 {
+
+
+
+		public static void main(String[] args) {
+
+		// TODO Auto-generated method stub*/
+
+
+
+
+
+		int myNum = 5;
+
+		String website = "Rahul Shetty Academy";
+
+		char letter = 'r';
+
+		double dec = 5.99;
+
+		boolean myCard = true;
+
+
+
+		System.out.println(myNum+"is the value stored in the myNum variable");
+
+		System.out.println(website);
+
+		//Arrays -
+
+		int[] arr = new int[5];// 5, 10
+
+		arr[0] = 1;
+
+		arr[1] = 2;
+
+		arr[2]= 4;
+
+		arr[3]= 5;
+
+		arr[4]= 6;
+
+
+
+
+
+		int[] arr2 = {1,2,4,5,6};
+
+
+
+		System.out.println(arr2[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//for loop arr.length - 5
+
+		for(int i = 0; i< arr.length; i++) //4
+
+		{
+
+
+
+		System.out.println(arr[i]);
+
+		}
+
+
+
+		for (int i =0;i<arr2.length; i++)
+
+		{
+
+		System.out.println(arr2[i]);
+
+		}
+
+
+
+		String[] name = {"rahul", "shetty", "selenium"};
+
+
+
+		for(int i =0; i<name.length;i++)
+
+		{
+
+		System.out.println(name[i]);
+
+		}
+
+
+
+		for( String s: name)
+
+		{
+
+		System.out.println(s);
+
+		}
+
+		}
+
+	}
+
